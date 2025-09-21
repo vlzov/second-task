56446fafad6a66796da4475426ebd75996ee6cfe
Vladimir <vlzov36@gmail.com>
2025-09-21 21:08:06 +0530

    Add files via upload

---
+++ SecondTask.java
@@ -0,0 +1,9 @@
+class SecondTask {
+    static void main(String[] args) {
+        int income = 44000;
+        int spending = 30000;
+
+        System.out.println("Итого (руб):");
+        System.out.println(income - spending);
+    }
+}
